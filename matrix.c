550c980798b5e036c1f7ee8ab02de1be5618d76f
yz2cm <yz2cmjp@gmail.com>
2022-11-20 23:05:15 +0900

    matrixの全セルをWhiteで初期化した後に、Blackのセルをプロットするように修正

--- matrix.c
+++ matrix.c
@@ -17,6 +17,14 @@ matrix_t *matrix_new(size_t width, size_t height) {
     return matrix;
 }
 
+void matrix_fill_with(matrix_t *matrix, cell_color_t color) {
+    for (size_t y = 0; y < matrix->height; ++y) {
+        for (size_t x = 0; x < matrix->width; ++x) {
+            matrix->colors[y * matrix->width + x] = color;
+        }
+    }
+}
+
 void matrix_set_cell(matrix_t *matrix, size_t x, size_t y, cell_color_t color) {
     if (matrix == NULL) {
         return;
@@ -43,29 +51,30 @@ matrix_line_list_t *matrix_split(const matrix_t *matrix) {
 
 matrix_t* matrix_from(const black_point_list_t *list) {
 
-    const point_t max = point_new_noheap(black_point_list_max_x(list), black_point_list_max_y(list));
-    const point_t min = point_new_noheap(black_point_list_min_x(list), black_point_list_min_y(list));
-
     matrix_t *matrix;
     {
+        const point_t max = point_new_noheap(black_point_list_max_x(list), black_point_list_max_y(list));
+        const point_t min = point_new_noheap(black_point_list_min_x(list), black_point_list_min_y(list));
         const size_t width =  (size_t)max.x - (size_t)min.x + 1;
         const size_t height = (size_t)max.y - (size_t)min.y + 1;
         matrix = matrix_new(width, height);
     }
 
-    for(int32_t y = min.y; y <= max.y; ++y) {
-        for(int32_t x = min.x; x <= max.x; ++x) {
-            const size_t offset_x = (size_t)x - (size_t)min.x;
-            const size_t offset_y = (size_t)y - (size_t)min.y;
-
-            if (black_point_list_contains(list, point_new_noheap(x, y))) {
-                matrix_set_cell(matrix, offset_x, offset_y, BLACK);
-            } else {
-                matrix_set_cell(matrix, offset_x, offset_y, WHITE);
-            }
-        }
-    }
+    matrix_fill_with(matrix, WHITE);
+    matrix_plot_points(matrix, list, BLACK);
 
     return matrix;
 }
 
+void matrix_plot_points(matrix_t *matrix, const black_point_list_t *list, cell_color_t color) {
+
+    const point_t min = point_new_noheap(black_point_list_min_x(list), black_point_list_min_y(list));
+
+    for (size_t i = 0; i < list->length; ++i) {
+        const point_t *a_point = list->points + i;
+        const size_t offset_x = (size_t)a_point->x - (size_t)min.x;
+        const size_t offset_y = (size_t)a_point->y - (size_t)min.y;
+
+        matrix_set_cell(matrix, offset_x, offset_y, color);
+    }
+}
--- matrix.h
+++ matrix.h
@@ -4,8 +4,10 @@
 #include "common.h"
 
 matrix_t           *matrix_new(size_t width, size_t height);
+void                matrix_fill_with(matrix_t *matrix, cell_color_t color);
 void                matrix_set_cell(matrix_t *matrix, size_t x, size_t y, cell_color_t color);
 matrix_line_list_t *matrix_split(const matrix_t *matrix);
 matrix_t*           matrix_from(const black_point_list_t *list);
+void                matrix_plot_points(matrix_t *matrix, const black_point_list_t *list, cell_color_t color);
 
 #endif

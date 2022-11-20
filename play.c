3cbc1a57f520a7a49392b90d79c788081a04f59f
yz2cm <yz2cmjp@gmail.com>
2022-11-21 00:55:14 +0900

    Whiteセル上のant、Blackセル上のant、それぞれの行動を別関数に分割

--- play.c
+++ play.c
@@ -2,30 +2,35 @@
 #include "black_point_list.h"
 #include "ant.h"
 
-ant_t play_once(const ant_t *ant, black_point_list_t *list) {
-    
-    if (black_point_list_contains(list, ant->point)) {
-        const ant_t ant_rotated = ant_rotate_left(ant);
-        black_point_list_remove(list, ant_rotated.point);
-        const ant_t ant_moved = ant_move_forward(&ant_rotated);
-
-        return ant_moved;
-    } else {
-        const ant_t ant_rotated = ant_rotate_right(ant);
-        black_point_list_add(list, ant_rotated.point);
-        const ant_t ant_moved = ant_move_forward(&ant_rotated);
-
-        return ant_moved;
-    }
-}
-
 black_point_list_t *play(uint32_t step) {
     ant_t ant = ant_new();
     black_point_list_t *list = black_point_list_new();
 
     for (size_t i = 0; i < step; ++i) {
-        ant = play_once(&ant, list);
+        if (black_point_list_contains(list, ant.point)) {
+            ant = play_ant_on_black(&ant, list);
+        } else {
+            ant = play_ant_on_white(&ant, list);
+        }
     }
 
     return list;
+}
+
+ant_t play_ant_on_black(const ant_t *ant, black_point_list_t *list) {
+
+        const ant_t ant_rotated = ant_rotate_left(ant);
+        black_point_list_remove(list, ant_rotated.point);
+        const ant_t ant_moved = ant_move_forward(&ant_rotated);
+
+        return ant_moved;
+}
+
+ant_t play_ant_on_white(const ant_t *ant, black_point_list_t *list) {
+
+        const ant_t ant_rotated = ant_rotate_right(ant);
+        black_point_list_add(list, ant_rotated.point);
+        const ant_t ant_moved = ant_move_forward(&ant_rotated);
+
+        return ant_moved;
 }
--- play.h
+++ play.h
@@ -2,7 +2,9 @@
 #define __YZ2CM_LANGTON_ANT_PLAY_H__
 #include "common.h"
 
-ant_t               play_once(const ant_t *ant, black_point_list_t *list);
+
 black_point_list_t *play(uint32_t step);
+ant_t               play_ant_on_black(const ant_t *ant, black_point_list_t *list);
+ant_t               play_ant_on_white(const ant_t *ant, black_point_list_t *list);
 
 #endif
